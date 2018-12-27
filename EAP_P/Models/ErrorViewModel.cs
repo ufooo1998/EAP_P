8a404096ddd21102f9389d221cdf861ee0e66d72
ufooo1998 <yurnero1681998@gmail.com>
2018-12-27 10:01:53 +0700

    Add project files.

---
+++ EAP_P/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace EAP_P.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
